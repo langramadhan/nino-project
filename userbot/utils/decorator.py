56238113e163dca5ff7a86ddde33e4a502853a63
langramadhan <100834738+langramadhan@users.noreply.github.com>
2022-03-05 13:48:36 +0700

    Update decorator.py

--- userbot/utils/decorator.py
+++ userbot/utils/decorator.py
@@ -14,12 +14,6 @@ from userbot import (
     CMD_HANDLER,
     CMD_LIST,
     LOAD_PLUG,
-    MAN2,
-    MAN3,
-    MAN4,
-    MAN5,
-    SUDO_HANDLER,
-    SUDO_USERS,
     bot,
     tgbot,
 )
@@ -103,44 +97,8 @@ def man_cmd(
                         **args, from_users=list(SUDO_USERS), pattern=sudo_reg
                     ),
                 )
-        if MAN2:
-            if not disable_edited:
-                MAN2.add_event_handler(
-                    func, events.MessageEdited(**args, outgoing=True, pattern=man_reg)
-                )
-            MAN2.add_event_handler(
-                func, events.NewMessage(**args, outgoing=True, pattern=man_reg)
-            )
-        if MAN3:
-            if not disable_edited:
-                MAN3.add_event_handler(
-                    func, events.MessageEdited(**args, outgoing=True, pattern=man_reg)
-                )
-            MAN3.add_event_handler(
-                func, events.NewMessage(**args, outgoing=True, pattern=man_reg)
-            )
-        if MAN4:
-            if not disable_edited:
-                MAN4.add_event_handler(
-                    func, events.MessageEdited(**args, outgoing=True, pattern=man_reg)
-                )
-            MAN4.add_event_handler(
-                func, events.NewMessage(**args, outgoing=True, pattern=man_reg)
-            )
-        if MAN5:
-            if not disable_edited:
-                MAN5.add_event_handler(
-                    func, events.MessageEdited(**args, outgoing=True, pattern=man_reg)
-                )
-            MAN5.add_event_handler(
-                func, events.NewMessage(**args, outgoing=True, pattern=man_reg)
-            )
-        try:
-            LOAD_PLUG[file_test].append(func)
-        except Exception:
-            LOAD_PLUG.update({file_test: [func]})
-        return func
-
+        
+    return func
     return decorator
 
 
@@ -150,14 +108,6 @@ def man_handler(
     def decorator(func):
         if bot:
             bot.add_event_handler(func, events.NewMessage(**args, incoming=True))
-        if MAN2:
-            MAN2.add_event_handler(func, events.NewMessage(**args, incoming=True))
-        if MAN3:
-            MAN3.add_event_handler(func, events.NewMessage(**args, incoming=True))
-        if MAN4:
-            MAN4.add_event_handler(func, events.NewMessage(**args, incoming=True))
-        if MAN5:
-            MAN5.add_event_handler(func, events.NewMessage(**args, incoming=True))
         return func
 
     return decorator
@@ -182,14 +132,6 @@ def chataction(**args):
     def decorator(func):
         if bot:
             bot.add_event_handler(func, events.ChatAction(**args))
-        if MAN2:
-            MAN2.add_event_handler(func, events.ChatAction(**args))
-        if MAN3:
-            MAN3.add_event_handler(func, events.ChatAction(**args))
-        if MAN4:
-            MAN4.add_event_handler(func, events.ChatAction(**args))
-        if MAN5:
-            MAN5.add_event_handler(func, events.ChatAction(**args))
         return func
 
     return decorator
