5ade3c0057bd914a6cc1dc32a20896523a6da634
Lang Ramadhan <100834738+langramadhan@users.noreply.github.com>
2022-03-06 20:55:51 +0700

    Update misc.py

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -139,7 +139,7 @@ async def repeat(rep):
 async def repo_is_here(wannasee):
     """ For .repo command, just returns the repo URL. """
     await wannasee.edit(
-        "Click [here](https://github.com/sahyam2019/oub-remix) to open oub-remix's GitHub page."
+        "Click [here](https://github.com/langramadhan/nino-project) to open Nino Project GitHub page."
     )
 
     
