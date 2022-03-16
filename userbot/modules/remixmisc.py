d1247ae1c8f0df42a12ddc08b067d29d7efe6910
Lang Ramadhan <100834738+langramadhan@users.noreply.github.com>
2022-03-16 07:52:25 +0700

    Update remixmisc.py

--- userbot/modules/remixmisc.py
+++ userbot/modules/remixmisc.py
@@ -800,7 +800,7 @@ async def _(event):
     await event.edit("succesfully reverted to your account back")
 
 CMD_HELP.update({
-    "remixmisc":
+    "ninomisc":
     "`.app`\
 \nUsage: type .app name and get app details.\
 \n\n`.undlt`\
