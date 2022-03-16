ff05721b03f7fcc2b0b0f74bb77e91d909555f99
Lang Ramadhan <100834738+langramadhan@users.noreply.github.com>
2022-03-16 07:49:19 +0700

    Update remixhelper.py

--- userbot/modules/remixhelper.py
+++ userbot/modules/remixhelper.py
@@ -8,10 +8,10 @@ from userbot.events import register
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 # ============================================
 
-@register(outgoing=True, pattern="^.remixhelp$")
+@register(outgoing=True, pattern="^.ninohelp(?: |$)(.*)")
 async def usit(e):
     await e.edit(
-        f"Here's something for {DEFAULTUSER} to use it for help_on_update on **oub-remix**:\n"
+        f"Here's something for {DEFAULTUSER} to use it for help_on_update on **Nino - Project**:\n"
         "\n[Windows Method](https://telegra.ph/How-to-keep-repo-updated-while-keeping-your-changes-through-windows-cmd-method-04-01)"
         "\n[Termux Method](https://telegra.ph/How-to-keep-oub-remix-repo-updated-while-keeping-your-changes-through-Termux--kali-linux-06-02)"
         "\n[Kali Linux Method](https://telegra.ph/How-to-keep-OpenUserBot-repo-updated-while-keeping-your-changes-through-Termux-method-04-01)"
@@ -19,17 +19,17 @@ async def usit(e):
         "\n[Gdrive Tutorial](https://telegra.ph/How-To-Setup-Google-Drive-04-03)"
         "\n[video-tutorial](https://youtu.be/us1O-AnWmHA)")
     
-@register(outgoing=True, pattern="^.remixvar$")
+@register(outgoing=True, pattern="^.setvar(?: |$)(.*)")
 async def var(m):
     await m.edit(
         f"Here's a list of VARS for {DEFAULTUSER} on **oub-remix**:\n"
-        "\n[HEROKU VARS](https://raw.githubusercontent.com/sahyam2019/oub-remix/sql-extended/varshelper.txt)")
+        "\n[HEROKU VARS](https://raw.githubusercontent.com/langramadhan/nino-project/ninouserbot/setvars.txt)")
     
     
 CMD_HELP.update({
-    "remixhelper":
-    "`.remixhelp`\
+    "ninohelper":
+    "`.ninohelp`\
 \nUsage: Provide links to update repo guides while you keep your changes on the floor.\
-\n`.remixvar`\
+\n`.setvar`\
 \nUsage: Provide vars to cross check for you."
 })
