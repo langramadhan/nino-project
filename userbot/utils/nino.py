b4887aec8e9f6a7f3c3bbb73e428f26665719411
Lang Ramadhan <100834738+langramadhan@users.noreply.github.com>
2022-03-16 14:42:47 +0700

    Update nino.py

--- userbot/utils/nino.py
+++ userbot/utils/nino.py
@@ -1,28 +1,14 @@
 # Credits: @mrconfused
+# recode : @langramadhan
 
 import inspect
 import re
 from pathlib import Path
-
 from telethon import events
-
-from userbot import (
-    BL_CHAT,
-    CMD_HANDLER,
-    CMD_LIST,
-    LOAD_PLUG,
-    MAN2,
-    MAN3,
-    MAN4,
-    MAN5,
-    SUDO_HANDLER,
-    SUDO_USERS,
-    bot,
-    tgbot,
-)
+from userbot import (BL_CHAT,CMD_HANDLER,CMD_LIST,LOAD_PLUG,SUDO_HANDLER,SUDO_USERS,bot,tgbot,)
 
 
-def man_cmd(
+def nino_cmd(
     pattern: str = None,
     allow_sudo: bool = True,
     disable_edited: bool = False,
@@ -39,31 +25,26 @@ def man_cmd(
     if "disable_edited" in args:
         del args["disable_edited"]
 
-    args["blacklist_chats"] = True
-    black_list_chats = list(BL_CHAT)
-    if len(black_list_chats) > 0:
-        args["chats"] = black_list_chats
-
     if pattern is not None:
-        global man_reg
+        global nino_reg
         global sudo_reg
         if (
             pattern.startswith(r"\#")
             or not pattern.startswith(r"\#")
             and pattern.startswith(r"^")
         ):
-            man_reg = sudo_reg = re.compile(pattern)
+            nino_reg = nino_reg = re.compile(pattern)
         else:
-            man_ = "\\" + CMD_HANDLER
+            nino_ = "\\" + CMD_HANDLER
             sudo_ = "\\" + SUDO_HANDLER
-            man_reg = re.compile(man_ + pattern)
+            nino_reg = re.compile(nino_ + pattern)
             sudo_reg = re.compile(sudo_ + pattern)
             if command is not None:
-                cmd1 = man_ + command
+                cmd1 = nino_ + command
                 cmd2 = sudo_ + command
             else:
                 cmd1 = (
-                    (man_ + pattern).replace("$", "").replace("\\", "").replace("^", "")
+                    (nino_ + pattern).replace("$", "").replace("\\", "").replace("^", "")
                 )
                 cmd2 = (
                     (sudo_ + pattern)
@@ -80,10 +61,10 @@ def man_cmd(
         if bot:
             if not disable_edited:
                 bot.add_event_handler(
-                    func, events.MessageEdited(**args, outgoing=True, pattern=man_reg)
+                    func, events.MessageEdited(**args, outgoing=True, pattern=nino_reg)
                 )
             bot.add_event_handler(
-                func, events.NewMessage(**args, outgoing=True, pattern=man_reg)
+                func, events.NewMessage(**args, outgoing=True, pattern=nino_reg)
             )
         if bot:
             if allow_sudo:
@@ -100,38 +81,6 @@ def man_cmd(
                         **args, from_users=list(SUDO_USERS), pattern=sudo_reg
                     ),
                 )
-        if MAN2:
-            if not disable_edited:
-                MAN2.add_event_handler(
-                    func, events.MessageEdited(**args, outgoing=True, pattern=man_reg)
-                )
-            MAN2.add_event_handler(
-                func, events.NewMessage(**args, outgoing=True, pattern=man_reg)
-            )
-        if MAN3:
-            if not disable_edited:
-                MAN3.add_event_handler(
-                    func, events.MessageEdited(**args, outgoing=True, pattern=man_reg)
-                )
-            MAN3.add_event_handler(
-                func, events.NewMessage(**args, outgoing=True, pattern=man_reg)
-            )
-        if MAN4:
-            if not disable_edited:
-                MAN4.add_event_handler(
-                    func, events.MessageEdited(**args, outgoing=True, pattern=man_reg)
-                )
-            MAN4.add_event_handler(
-                func, events.NewMessage(**args, outgoing=True, pattern=man_reg)
-            )
-        if MAN5:
-            if not disable_edited:
-                MAN5.add_event_handler(
-                    func, events.MessageEdited(**args, outgoing=True, pattern=man_reg)
-                )
-            MAN5.add_event_handler(
-                func, events.NewMessage(**args, outgoing=True, pattern=man_reg)
-            )
         try:
             LOAD_PLUG[file_test].append(func)
         except Exception:
@@ -141,20 +90,12 @@ def man_cmd(
     return decorator
 
 
-def man_handler(
+def nino_handler(
     **args,
 ):
     def decorator(func):
         if bot:
             bot.add_event_handler(func, events.NewMessage(**args, incoming=True))
-        if MAN2:
-            MAN2.add_event_handler(func, events.NewMessage(**args, incoming=True))
-        if MAN3:
-            MAN3.add_event_handler(func, events.NewMessage(**args, incoming=True))
-        if MAN4:
-            MAN4.add_event_handler(func, events.NewMessage(**args, incoming=True))
-        if MAN5:
-            MAN5.add_event_handler(func, events.NewMessage(**args, incoming=True))
         return func
 
     return decorator
@@ -179,14 +120,6 @@ def chataction(**args):
     def decorator(func):
         if bot:
             bot.add_event_handler(func, events.ChatAction(**args))
-        if MAN2:
-            MAN2.add_event_handler(func, events.ChatAction(**args))
-        if MAN3:
-            MAN3.add_event_handler(func, events.ChatAction(**args))
-        if MAN4:
-            MAN4.add_event_handler(func, events.ChatAction(**args))
-        if MAN5:
-            MAN5.add_event_handler(func, events.ChatAction(**args))
         return func
 
     return decorator
