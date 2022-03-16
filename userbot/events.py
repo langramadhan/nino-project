f9da29890d3157f01b18d13f9cb5c732fadc69f3
Lang Ramadhan <100834738+langramadhan@users.noreply.github.com>
2022-03-16 15:39:44 +0700

    Update events.py

--- userbot/events.py
+++ userbot/events.py
@@ -18,47 +18,7 @@ from telethon import events
 
 from userbot import bot, BOTLOG_CHATID, LOGSPAMMER, CMD_HANDLER, CMD_LIST, SUDO_HANDLER, SUDO_USERS
 
-def register(**args):
-    """ Register a new event. """
-    pattern = args.get('pattern', None)
-    disable_edited = args.get('disable_edited', True)
-    ignore_unsafe = args.get('ignore_unsafe', False)
-    unsafe_pattern = r'^[^/!#@\$A-Za-z]'
-    groups_only = args.get('groups_only', False)
-    trigger_on_fwd = args.get('trigger_on_fwd', False)
-    disable_errors = args.get('disable_errors', False)
-    insecure = args.get('insecure', False)
-    trigger_on_inline = args.get('trigger_on_inline', False)
-
-    if pattern is not None and not pattern.startswith('(?i)'):
-        args['pattern'] = '(?i)' + pattern
-
-    if "disable_edited" in args:
-        del args['disable_edited']
-
-    if "ignore_unsafe" in args:
-        del args['ignore_unsafe']
-
-    if "groups_only" in args:
-        del args['groups_only']
-
-    if "disable_errors" in args:
-        del args['disable_errors']
-
-    if "trigger_on_fwd" in args:
-        del args['trigger_on_fwd']
-
-    if "insecure" in args:
-        del args['insecure']
-
-    if "trigger_on_inline" in args:
-        del args['trigger_on_inline']
-
-    if pattern:
-        if not ignore_unsafe:
-            args['pattern'] = pattern.replace('^.', unsafe_pattern, 1)
-            
-    def nino_cmd(
+   def nino_cmd(
     pattern: str = None,
     allow_sudo: bool = True,
     disable_edited: bool = False,
@@ -178,6 +138,47 @@ def callback(**args):
         return func
 
     return decorator
+
+
+def register(**args):
+    """ Register a new event. """
+    pattern = args.get('pattern', None)
+    disable_edited = args.get('disable_edited', True)
+    ignore_unsafe = args.get('ignore_unsafe', False)
+    unsafe_pattern = r'^[^/!#@\$A-Za-z]'
+    groups_only = args.get('groups_only', False)
+    trigger_on_fwd = args.get('trigger_on_fwd', False)
+    disable_errors = args.get('disable_errors', False)
+    insecure = args.get('insecure', False)
+    trigger_on_inline = args.get('trigger_on_inline', False)
+
+    if pattern is not None and not pattern.startswith('(?i)'):
+        args['pattern'] = '(?i)' + pattern
+
+    if "disable_edited" in args:
+        del args['disable_edited']
+
+    if "ignore_unsafe" in args:
+        del args['ignore_unsafe']
+
+    if "groups_only" in args:
+        del args['groups_only']
+
+    if "disable_errors" in args:
+        del args['disable_errors']
+
+    if "trigger_on_fwd" in args:
+        del args['trigger_on_fwd']
+
+    if "insecure" in args:
+        del args['insecure']
+
+    if "trigger_on_inline" in args:
+        del args['trigger_on_inline']
+
+    if pattern:
+        if not ignore_unsafe:
+            args['pattern'] = pattern.replace('^.', unsafe_pattern, 1)
 
     def decorator(func):
         async def wrapper(check):
