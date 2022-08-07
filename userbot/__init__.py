5fd4fbcf723d6e821e4b913d5a0d5d709d6f13ef
Lang Ramadhan <100834738+langramadhan@users.noreply.github.com>
2022-08-07 18:31:47 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -14,7 +14,6 @@ from logging import basicConfig, getLogger, INFO, DEBUG
 from distutils.util import strtobool as sb
 from math import ceil
 
-from pySmartDL import SmartDL
 from pymongo import MongoClient
 from redis import StrictRedis
 from dotenv import load_dotenv
